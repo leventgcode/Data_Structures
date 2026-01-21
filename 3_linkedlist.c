2ee5b545af1a5bf5f6639a0ccf78417d14476f39
leventgcode <levent123453@gmail.com>
2026-01-21 16:31:24 +0300

    update

--- 3_linkedlist.c
+++ 3_linkedlist.c
@@ -36,12 +36,66 @@ else{
     return 1;
 }
 
+//Add Note to Head
+int addNodeHead(int data)
 
+{ if(head == NULL){
+
+    struct node *new = (struct node*)malloc(sizeof(struct node));
+    new->data = data;
+    new->next = NULL;
+
+    head = tail = new;  
+}
+else{
+    struct node *new = (struct node*)malloc(sizeof(struct node));
+    new->data = data;
+    new->next = head;
+
+    head = new;}
+}
+
+//Print
+int print(){
+    struct node *index = head;
+    while(index != NULL){
+        printf("%d\n", index->data);
+        index = index->next;
+    }
+    return 1;
+}
+
+//Delete Node
+int delete(int data){
+    struct node *index = head;
+    struct node *prev = NULL;
+
+    while(index != NULL){
+        if(index->data == data){
+            if(prev == NULL){
+                head = index->next;
+                free(index);
+                return 1;
+            }
+            else{
+                prev->next = index->next;
+                free(index);
+                return 1;
+            }
+        }
+        prev = index;
+        index = index->next;
+    }
+    return -1; //Not Found
+}
 int main(){
 
     addNode(10);
     addNode(20);
     addNode(30);
 
+    addNodeHead(15);
+
+    print();
     return 1;
 }
